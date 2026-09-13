a34c2686fc36015988a48f9eef9519dca880e088
Krishna Kumar Shah <krishnakumarshah005@gmail.com>
2026-09-13 11:31:50 +0530

    Update load_Silver.sql

--- scripts/Silver/load_Silver.sql
+++ scripts/Silver/load_Silver.sql
@@ -1,1 +1,68 @@
+/*
+Analayze
+  */
+SELECT *
+FROM Bronze.crm_cust_info;
+
+/*
+
+Inserting into Silver after Data Cleansing
+
+*/
+
+INSERT INTO Silver.crm_cust_info
+(
+cst_id,
+cst_key,
+cst_firstname,
+cst_lastname,
+cst_marital_status,
+cst_gndr,
+cst_create_date
+)
+SELECT 
+cst_id,
+cst_key,
+COALESCE(TRIM(cst_firstname),'') cst_firstname,
+COALESCE(TRIM(cst_lastName),'') cst_lastname,
+CASE WHEN TRIM(cst_marital_status) = 'S' THEN 'Single'
+	 WHEN TRIM(cst_marital_status) = 'M' THEN 'Married'
+	 ELSE 'Relationship'
+END cst_marital_status
+,
+CASE WHEN TRIM(cst_gndr) = 'M' THEN 'Male'
+	 WHEN TRIM(cst_gndr) = 'F' THEN 'Female'
+	 ELSE 'Other'
+END cst_gndr
+,
+cst_create_date
+FROM
+(
+	SELECT 
+	*,
+	ROW_NUMBER() OVER(PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last 
+	FROM Bronze.crm_cust_info
+)t
+WHERE flag_last=1;
+
+/*
+Analyze
+*/
+
+SELECT *
+FROM Silver.crm_cust_info;
+
+/*
+Checking quality
+
+*/
+
+SELECT cst_id, COUNT(*)
+FROM Silver.crm_cust_info 
+WHERE cst_marital_status IS NULL
+OR cst_gndr IS NULL
+OR cst_firstname != TRIM(cst_firstname)
+OR cst_lastname != TRIM(cst_lastname)
+GROUP BY cst_id
+HAVING COUNT(*)>1;
 
