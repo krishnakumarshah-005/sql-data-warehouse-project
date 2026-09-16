197dff267b24c3c26bdd038534efa5bbc506cf46
Krishna Kumar Shah <krishnakumarshah005@gmail.com>
2026-09-16 09:30:46 +0530

    Update load_Silver.sql

--- scripts/Silver/load_Silver.sql
+++ scripts/Silver/load_Silver.sql
@@ -71,20 +71,71 @@ HAVING COUNT(*)>1;
 
 /*
 Cleansing of Bronze.crm_prd_info
+and loading in Silver.crm_prd_info
 */
 
-SELECT *
-FROM Bronze.crm_prd_info;
+USE DataWareHouse;
 
-SELECT 
+IF OBJECT_ID('Silver.crm_prd_info','U') IS NOT NULL
+DROP TABLE Silver.crm_prd_info;
+CREATE TABLE Silver.crm_prd_info(
+prd_id INT,
+prd_key NVARCHAR(50),
+cat_id NVARCHAR(50),
+prd_nm   NVARCHAR(50),
+prd_cost INT,
+prd_line  NVARCHAR(50),
+prd_start_dt   DATE,
+prd_end_dt  DATE,
+dwh_create_dt   DATETIME2 DEFAULT GETDATE()
+);
+
+
+INSERT INTO Silver.crm_prd_info(
 prd_id,
 prd_key,
-REPLACE(SUBSTRING(prd_key,1,5),'-','_') AS cat_id,
-SUBSTRING(prd_key,7,LEN(prd_key)) AS prd_key,
+cat_id,
 prd_nm,
-ISNULL(prd_cost,0) AS prd_cost1,
--- COALESCE(prd_cost,0) AS prd_cost2,
-COALESCE(prd_line,'S'),
+prd_cost,
+prd_line,
 prd_start_dt,
-COALESCE(prd_end_dt,GETDATE())
+prd_end_dt
+)
+SELECT 
+prd_id,
+SUBSTRING(prd_key,7,LEN(prd_key)) AS prd_key,
+REPLACE(SUBSTRING(prd_key,1,5),'-','_') AS cat_id,
+prd_nm,
+ISNULL(prd_cost,0) AS prd_cost,
+-- COALESCE(prd_cost,0) AS prd_cost1,
+CASE
+	WHEN UPPER(TRIM(prd_line))='M' THEN 'Mountain'
+	WHEN UPPER(TRIM(prd_line))='R' THEN 'Road'
+	WHEN UPPER(TRIM(prd_line))='S' THEN 'Other Sales'
+	WHEN UPPER(TRIM(prd_line))='T' THEN 'Touring'
+	ELSE 'None'
+END AS prd_line,
+prd_start_dt,
+/*
+-- prd_end_dt,
+--CAST(LEAD(prd_start_dt) OVER(PARTITION BY prd_key ORDER BY prd_start_dt)-1 AS DATE) AS prd_end_dt1   error 
+CAST(
+    LEAD(CAST(prd_start_dt AS DATETIME)) OVER(PARTITION BY prd_key ORDER BY prd_start_dt) - 1
+    AS DATE
+) AS prd_end_dt1,
+LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt)-1 AS prd_end_dt
+*/
+CAST(
+CASE 
+    WHEN LEAD(prd_start_dt) OVER(PARTITION BY prd_key ORDER BY prd_start_dt) IS NULL 
+        THEN GETDATE()  -- fallback if no next row
+    WHEN LEAD(prd_start_dt) OVER(PARTITION BY prd_key ORDER BY prd_start_dt) <= prd_start_dt 
+        THEN prd_start_dt  -- ensures end_date is never less than start_date
+    ELSE DATEADD(DAY, -1, LEAD(prd_start_dt) OVER(PARTITION BY prd_key ORDER BY prd_start_dt))
+END  AS DATE) AS prd_end_dt
 FROM Bronze.crm_prd_info;
+
+
+
+SELECT *
+FROM Silver.crm_prd_info;
